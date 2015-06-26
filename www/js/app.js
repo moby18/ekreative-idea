d3549245322677dcaa4f5edcd5faf6e23245df17
Andrey <andrey@ekreative.com>
2015-06-26 17:22:12 +0300

    make css and js changes for ionic-material default template

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material'])
+angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material', 'ionMdInput'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -20,7 +20,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+
+        // Turn off caching for demo simplicity's sake
+        $ionicConfigProvider.views.maxCache(0);
+
+        /*
+         // Turn off back button text
+         $ionicConfigProvider.backButton.previousTitleText(false);
+         */
+
   $stateProvider
 
   .state('app', {
@@ -29,6 +38,93 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material'])
     templateUrl: "templates/menu.html",
     controller: 'AppCtrl'
   })
+
+      .state('app.activity', {
+          url: '/activity',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/activity.html',
+                  controller: 'ActivityCtrl'
+              },
+              'fabContent': {
+                  template: '<button id="fab-activity" class="button button-fab button-fab-top-right expanded button-energized-900 flap"><i class="icon ion-paper-airplane"></i></button>',
+                  controller: function ($timeout) {
+                      $timeout(function () {
+                          document.getElementById('fab-activity').classList.toggle('on');
+                      }, 200);
+                  }
+              }
+          }
+      })
+
+      .state('app.friends', {
+          url: '/friends',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/friends.html',
+                  controller: 'FriendsCtrl'
+              },
+              'fabContent': {
+                  template: '<button id="fab-friends" class="button button-fab button-fab-top-left expanded button-energized-900 spin"><i class="icon ion-chatbubbles"></i></button>',
+                  controller: function ($timeout) {
+                      $timeout(function () {
+                          document.getElementById('fab-friends').classList.toggle('on');
+                      }, 900);
+                  }
+              }
+          }
+      })
+
+      .state('app.gallery', {
+          url: '/gallery',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/gallery.html',
+                  controller: 'GalleryCtrl'
+              },
+              'fabContent': {
+                  template: '<button id="fab-gallery" class="button button-fab button-fab-top-right expanded button-energized-900 drop"><i class="icon ion-heart"></i></button>',
+                  controller: function ($timeout) {
+                      $timeout(function () {
+                          document.getElementById('fab-gallery').classList.toggle('on');
+                      }, 600);
+                  }
+              }
+          }
+      })
+
+      .state('app.login', {
+          url: '/login',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/login.html',
+                  controller: 'LoginCtrl'
+              },
+              'fabContent': {
+                  template: ''
+              }
+          }
+      })
+
+      .state('app.profile', {
+          url: '/profile',
+          views: {
+              'menuContent': {
+                  templateUrl: 'templates/profile.html',
+                  controller: 'ProfileCtrl'
+              },
+              'fabContent': {
+                  template: '<button id="fab-profile" class="button button-fab button-fab-bottom-right button-energized-900"><i class="icon ion-plus"></i></button>',
+                  controller: function ($timeout) {
+                      /*$timeout(function () {
+                       document.getElementById('fab-profile').classList.toggle('on');
+                       }, 800);*/
+                  }
+              }
+          }
+      })
+
+  //    native ionic part
 
   .state('app.search', {
     url: "/search",
@@ -65,7 +161,10 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-material'])
         controller: 'PlaylistCtrl'
       }
     }
+
+
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
+        //$urlRouterProvider.otherwise('/app/login');
 });
